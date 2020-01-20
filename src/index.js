2a9ea44bf06d547a589b47933eabf2c96fd885bd
Adepiper <52428747+Adepiper@users.noreply.github.com>
2020-01-21 00:17:41 +0100

    react snap

--- src/index.js
+++ src/index.js
@@ -5,11 +5,25 @@ import * as serviceWorker from './serviceWorker';
 import Root from './Root';
 import Firebase , {FirebaseContext} from './firebase'
 
-ReactDOM.render(
-      <FirebaseContext.Provider value = {new Firebase()} >
-        <Root />
-        </FirebaseContext.Provider>, 
-        document.getElementById('root'));
+const rootElement = document.getElementById('root')
+
+
+        if (rootElement.hasChildNodes()){
+          ReactDOM.hydrate(
+            <FirebaseContext.Provider value = {new Firebase()} >
+              <Root />
+              </FirebaseContext.Provider>, 
+              rootElement);
+        } else {
+          ReactDOM.render(
+            <FirebaseContext.Provider value = {new Firebase()} >
+              <Root />
+              </FirebaseContext.Provider>, 
+              rootElement);
+        }
+
+
+        
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
